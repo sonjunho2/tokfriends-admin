aa39a844542455cdbb6c4b929ba95629fbd0814b
sonjunho2 <sonjunho2@gmail.com>
2025-09-25 15:23:35 +0900

    Delete admin-api/src/auth/dto/login.dto.ts

--- admin-api/src/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class LoginDto {
-  @ApiProperty({ example: 'admin@local' })
-  email!: string;
-
-  @ApiProperty({ example: 'Admin123!' })
-  password!: string;
-}
