2d13af7cec037427582d4a1c8ed5de5e73de001d
sonjunho2 <sonjunho2@gmail.com>
2025-09-25 15:24:21 +0900

    Delete admin-api/src/announcements/announcements.module.ts

--- admin-api/src/announcements/announcements.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AnnouncementsService } from './announcements.service';
-import { AnnouncementsController } from './announcements.controller';
-
-@Module({
-  providers: [AnnouncementsService],
-  controllers: [AnnouncementsController],
-})
-export class AnnouncementsModule {}
