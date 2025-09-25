d16fa13dc7e5339f6b628e4fe4583aac7d55be5d
sonjunho2 <sonjunho2@gmail.com>
2025-09-25 15:22:26 +0900

    Delete admin-api/src/banned-words/banned-words.module.ts

--- admin-api/src/banned-words/banned-words.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BannedWordsService } from './banned-words.service';
-import { BannedWordsController } from './banned-words.controller';
-
-@Module({
-  providers: [BannedWordsService],
-  controllers: [BannedWordsController],
-})
-export class BannedWordsModule {}
