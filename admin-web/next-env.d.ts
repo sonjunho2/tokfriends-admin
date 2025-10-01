c9c2b4c56508b658ff2799ae85b2b580181631b8
sonjunho2 <sonjunho2@gmail.com>
2025-10-01 15:42:48 +0900

    Update next-env.d.ts

--- admin-web/next-env.d.ts
+++ admin-web/next-env.d.ts
@@ -1,10 +1,5 @@
-diff --git a//dev/null b/admin-web/next-env.d.ts
-index 0000000000000000000000000000000000000000..4f11a03dc6cc37f2b5105c08f2e7b24c603ab2f4 100644
---- a//dev/null
-+++ b/admin-web/next-env.d.ts
-@@ -0,0 +1,5 @@
-+/// <reference types="next" />
-+/// <reference types="next/image-types/global" />
-+
-+// NOTE: This file should not be edited
-+// see https://nextjs.org/docs/basic-features/typescript for more information.
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
