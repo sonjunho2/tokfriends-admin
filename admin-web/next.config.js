66f668c21bdd7436beb750263fd30153438ddb07
sonjunho2 <sonjunho2@gmail.com>
2025-12-22 14:09:10 +0900

    Update next.config.js

--- admin-web/next.config.js
+++ admin-web/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  i18n: {
+    locales: ['ko', 'en'],
+    defaultLocale: 'ko',
+  },
 }
 
 module.exports = nextConfig
